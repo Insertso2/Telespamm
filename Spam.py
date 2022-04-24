d61e87f0235ca601eef2a755ca36fade97e05f88
Insertso2 <101923434+Insertso2@users.noreply.github.com>
2022-04-25 01:05:41 +0300

    Update Spam.py

--- Spam.py
+++ Spam.py
@@ -3,7 +3,7 @@ import random
 
 token2 = '5195249320:AAHbVHkq3yyj2rRl2Ueesdzn-VctgZ4ijLc' 
 bot = telebot.TeleBot(input('введите токен бота'))
-messages[]
+
 Chat_id = int(input('введите id чата')) 
 User_input = input('введите сообщение')
 
